07f53e63a7dbdd0e49e8ed3b010cb0cb010270a9
hendrianiyan <hendrian582@gmail.com>
2022-04-23 11:19:31 +0700

    membuat project baru

---
+++ Controllers/Movie.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mod8_1302200104.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Movie : ControllerBase
+    {
+        private string[] arrTopmovie = new[]
+        {
+             "The Shawshank Redemption",
+             "The Godfather",
+             "The Dark Knight"
+        };
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return arrTopmovie;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return arrTopmovie[id];
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Mod8_1302200104
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
